3405724224201df28968829bc0dc17c217a1ce2a
Alexander Fakhrudinov <asbuka@gmail.com>
2022-01-20 06:06:15 +0300

    dz7

---
+++ DZ7_coder_decoder/CoderDecoder/CoderDecoder/Program.cs
@@ -0,0 +1,46 @@
+﻿using EncodeDecodeLib.Interfaces;
+using EncodeDecodeLib.Realisation;
+
+/*
+ * Определить интерфейс IСoder, который полагает методы поддержки шифрования строк. 
+ * В интерфейсе объявляются два метода Encode() и Decode(), используемые для шифрования и дешифрования строк. 
+ * 
+ * Создать класс ACoder, реализующий интерфейс ICoder. 
+ * Класс шифрует строку посредством сдвига каждого символа на одну «алфавитную» позицию выше. 
+ * (В результате такого сдвига буква А становится буквой Б). 
+ * 
+ * Создать класс BCoder, реализующий интерфейс ICoder. 
+ * Класс шифрует строку, выполняя замену каждой буквы, 
+ * стоящей в алфавите на i-й позиции, на букву того же регистра, расположенную в алфавите на i-й позиции с конца алфавита. 
+ * (Например, буква В заменяется на букву Э. 
+ * 
+ * Написать программу, демонстрирующую функционирование классов).
+ */
+
+string stringInitial = "В чащах юга жил бы цитрус? Да, но фальшивый экземпляр! The quick brown fox jumps over the lazy dog.";
+
+Console.WriteLine("ACoder:");
+IСoder aa = new ACoder();
+Console.WriteLine("Initial=" + stringInitial);
+
+string stringEncodedA = aa.Encode(stringInitial);
+Console.WriteLine("Encoded=" + stringEncodedA);
+
+string stringDecodedA = aa.Decode(stringEncodedA);
+Console.WriteLine("Decoded=" + stringDecodedA);
+
+Console.WriteLine("Compare string is " + stringInitial.Equals(stringDecodedA));
+
+
+Console.WriteLine("\nBCoder:");
+IСoder bb = new BCoder();
+Console.WriteLine("Initial=" + stringInitial);
+
+string stringEncodedB = bb.Encode(stringInitial);
+Console.WriteLine("Encoded=" + stringEncodedB);
+
+string stringDecodedB = bb.Decode(stringEncodedB);
+Console.WriteLine("Decoded=" + stringDecodedB);
+
+Console.WriteLine("Compare string is " + stringInitial.Equals(stringDecodedB));
+
---
+++ DZ7_coder_decoder/CoderDecoder/EncodeDecodeLib/Realisation/BCoder.cs
@@ -0,0 +1,66 @@
+﻿using EncodeDecodeLib.Abstraction;
+using EncodeDecodeLib.CodeTables;
+
+namespace EncodeDecodeLib.Realisation
+{
+    public sealed class BCoder : CoderDecoderAbstract
+    {
+        public sealed override string Decode(string textToDecode)
+        {
+            string result = "";
+
+            foreach (char chr in textToDecode)
+            {
+                bool isFoundedInTable = false;
+
+                foreach (string codeTable in CodeTable.CodeTables)
+                {
+                    if (codeTable.Contains(chr))
+                    {
+                        result = result + GetSymbolFromCodeTable(codeTable, chr);
+                        isFoundedInTable = true;
+                        break;
+                    }
+                }
+
+                if (!isFoundedInTable)
+                {
+                    result = result + chr;
+                }
+            }
+
+            return result;
+        }
+
+        public sealed override string Encode(string textToEncode)
+        {
+            string result = "";
+
+            foreach (char chr in textToEncode)
+            {
+                bool isFoundedInTable = false;
+
+                foreach (string codeTable in CodeTable.CodeTables)
+                {
+                    if (codeTable.Contains(chr))
+                    {
+                        result = result + GetSymbolFromCodeTable(codeTable, chr);
+                        isFoundedInTable = true;
+                    }
+                }
+
+                if (!isFoundedInTable)
+                {
+                    result = result + chr;
+                }
+            }
+
+            return result;
+        }
+
+        private char GetSymbolFromCodeTable(string codeTable, char chr)
+        {
+            return codeTable[codeTable.Length - (codeTable.IndexOf(chr) + 1)];
+        }
+    }
+}
